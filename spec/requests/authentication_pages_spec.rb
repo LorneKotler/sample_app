1c6966f66cdc5fa2ae27e248f7d698f0dabf3a08
Lorne <kotler.lorne@gmail.com>
2012-08-01 21:17:44 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -67,6 +67,18 @@ describe "authorization" do
         end
       end
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
+        end
+      end
+
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -74,6 +86,7 @@ describe "authorization" do
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
+
 
         describe "after signing in" do
 
@@ -114,6 +127,17 @@ describe "authorization" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+
+          describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
       end
     end
 
